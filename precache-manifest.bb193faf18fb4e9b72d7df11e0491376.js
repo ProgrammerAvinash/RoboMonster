c3907d2d44fa87b0c92a0c29a259934179c4fc2c
ProgrammerAvinash <62152948+ProgrammerAvinash@users.noreply.github.com>
2020-05-13 02:19:01 +0530

    Updates

---
+++ precache-manifest.bb193faf18fb4e9b72d7df11e0491376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce73790fae414f89660edb8176607be",
+    "url": "/RoboMonster/index.html"
+  },
+  {
+    "revision": "ed9be1184fcd0eb06453",
+    "url": "/RoboMonster/static/css/main.94f3ed05.chunk.css"
+  },
+  {
+    "revision": "ca7d5ed62cb0bee9a392",
+    "url": "/RoboMonster/static/js/2.b76a97c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboMonster/static/js/2.b76a97c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed9be1184fcd0eb06453",
+    "url": "/RoboMonster/static/js/main.75c514b5.chunk.js"
+  },
+  {
+    "revision": "ce7838788cfafa7ab3a6",
+    "url": "/RoboMonster/static/js/runtime-main.424782a8.js"
+  }
+]);
